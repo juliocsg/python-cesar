bee9e00a059d023d6487aa065a6caf705f7f3d24
Julio César <jcesarsaucedo1993@gmail.com>
2019-04-09 17:52:38 -0400

    dictionary and if conditional

---
+++ Dictionary/change.py
@@ -0,0 +1,8 @@
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+thisdict["year"] = 2019
+print(thisdict)
+
---
+++ Dictionary/del2.py
@@ -0,0 +1,9 @@
+thisdict = {
+    "brand" : "Ford",
+    "model" : "Mustang",
+    "year" : 1964
+}
+del thisdict
+print(thisdict)
+#this will cause an error because "thislist"
+#don't exist
