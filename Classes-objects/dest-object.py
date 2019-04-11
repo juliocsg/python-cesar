9a666ea159f31a495b76261b5c72aedd891ccd0d
Julio César <jcesarsaucedo1993@gmail.com>
2019-04-11 17:53:27 -0400

    class, parent, child, iterator

---
+++ Classes-objects/dest-object.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+pl = Person("John", 36)
+
+del pl
+
+print(pl)
